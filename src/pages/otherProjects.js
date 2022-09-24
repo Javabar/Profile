dc24aca7228748a9e30ec5be65f0f37237e44954
Abdelbar Benaida <abdelbar94@hotmail.co.uk>
2022-09-24 19:20:55 +0100

    edited

---
+++ src/pages/otherProjects.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "../css/projects.css"
+import { BsGithub, BsFillPlayCircleFill } from "react-icons/bs";
+
+
+const OtherProjects = (props) => {
+    return (
+        <div className={"other-project"}>
+            <div className={"other-project-text"}>
+                <div className={"other-project-text-box"}>
+                    <h3>{props.title}</h3>
+                    <p>{props.desc}</p>
+                    <p className={"other-skills-text"}>{props.skills}</p>
+                </div>
+                <div className={"other-project-link"}>
+                    <a href={props.github} target="_blank" rel="noopener noreferrer">
+                    <BsGithub className="icon" />
+                    </a>
+                    <a href={props.website} target="_blank" rel="noopener noreferrer">
+                    <BsFillPlayCircleFill className="icon" />
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default OtherProjects;
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,18 +4,20 @@ import ProjectCodeStation from "./projectCodeStation";
 import ProjectWebsiteClone from "./projectWebsiteClone";
 import codestation from "../images/codestation.jpg";
 import websiteClone from "../images/websiteClone.jpg";
+import OtherProjects from "./otherProjects";
 
 
 const Project = () => {
     return ( 
         <div>
         <h1>Projects</h1>
-
+<h2>Featured Projects</h2>
+<div className={"projects1"}>
         <ProjectCodeStation
                     image={codestation}
                     title="Code Station"
                     desc="Code Station is a full-stack MERN application for the coding community. Users can signup or login to browse latest coding opportunities, get closer to the coding community, and log your interest so you never miss out."
-                    skills="JavaScript, React.js, CSS, MongoDB, Mongoose, Express, Node.js"
+                    skills="JavaScript, React.js, CSS, MongoDB, Mongoose, Express &amp;  Node.js"
                     github="https://github.com/Javabar/CN-CodeStation-38"
                     website="https://cn-codstation-38.netlify.app/"
                 />
@@ -27,10 +29,34 @@ const Project = () => {
                     skills="HTML &amp; CSS"
                     github="https://github.com/Javabar/Website-Clone"
                     website="https://website-clone-6e0cd4.netlify.app/"
-                    
                 />
-       
-            
+       </div>
+
+        <h2>Other Projects</h2>
+        <div className={"projects2"}>
+       <OtherProjects
+                    title="REST API"
+                    desc="A simple REST API with two databases."
+                    skills="MongoDB, Mongoose, Express &amp; JavaScript"
+                    github="https://github.com/Javabar/Rest-API"
+                />
+
+        <OtherProjects
+                    title="Cats4Life"
+                    desc="This was a small group project where we created a react.js application and generated API's."
+                    skills="GitHub, React.js, Rest API &amp; netlify"
+                    github="https://github.com/Javabar/CN-M38-cats4life"
+                    website="https://cn-m38-desk3-cats4life.netlify.app/"
+                />        
+
+{/* <OtherProjects
+                    title=""
+                    desc=""
+                    skills="HTML &amp; CSS"
+                    github=""
+                    website=""
+                />                */}
+        </div>
         </div>
      );
 }
