6450bea4db68f67541d44a97f943ab9ae452de11
Abdelbar Benaida <abdelbar94@hotmail.co.uk>
2022-09-25 22:59:54 +0100

    edited

--- src/pages/home.js
+++ src/pages/home.js
@@ -11,7 +11,7 @@ const Home = () => {
    
         <div>
         <h1>Hello! I am Abdelbar Benaida.</h1>
-        <h2 className="header2">I love to deign &amp; code</h2>
+        <h2 className="header2">I love to design &amp; code</h2>
       <p className="intro">I am an aspiring Software Developer from the UK. I enjoy building front-end and back-end applications.</p>
       <a className="btn" href="https://github.com/Javabar" target="blank">GitHub</a>
       <a className="btn" href="https://www.linkedin.com/in/abdelbar-benaida-aa25b982/" target="blank">LinkedIn</a>
